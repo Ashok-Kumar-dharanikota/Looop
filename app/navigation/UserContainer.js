5e02be28cd7ea74f62f80b6341d1cc3b35464a69
Ashok Kumar Dharanikota <ad001024092@techmahindra.com>
2025-06-07 14:02:50 +0530

    API: Able to fetch records data

--- app/navigation/UserContainer.js
+++ app/navigation/UserContainer.js
@@ -9,6 +9,22 @@ const UserContainer = () => {
   return (
     <User.Navigator
       screenOptions={{
+        tabBarStyle: {
+          height: 80,
+          borderRadius: 16,
+          marginLeft: 10,
+          marginRight: 10,
+          marginBottom: 10,
+          height: 80,
+          borderColor: 'white',
+          shadowColor: 'white',
+        },
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontWeight: 'bold',
+          color: 'black',
+        },
+        tabBarInactiveTintColor: 'gray',
         headerShown: false,
       }}>
       <User.Screen name="dashboard" component={Dashboard} />
